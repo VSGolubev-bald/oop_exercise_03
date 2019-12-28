2fc06410c58f9507bb1a7c4e96c06c82876b0925
VSGolubev-bald <vova.golubev.2000@mail.ru>
2019-12-28 18:38:28 +0300

    fixed

--- rhombus.cpp
+++ rhombus.cpp
@@ -4,21 +4,24 @@ rhombus::rhombus() : a_{0, 0}, b_{0, 0}, c_{0, 0}, d_{0, 0} {}
 rhombus::rhombus(const point &a, const point &b, const point &c, const point &d) : a_(a), b_(b), c_(c), d_(d) {};
 rhombus::rhombus(std::istream &is) {
     is >> a_ >> b_ >> c_ >> d_;
+    if ( VectPropX(operator-(a_, b_), operator-(d_, c_)) == VectPropY(operator-(a_, b_), operator-(d_, c_)) &&
+         VectPropX(operator-(a_, d_), operator-(b_, c_)) == VectPropY(operator-(a_, d_), operator-(b_, c_)) &&
+         ScalProd(operator-(c_,a_), operator-(d_,b_)) == 0 ) {
+        std::cout << "Correct" << std::endl;
+    }
+    else {
+        std::cout << "Wrong" << std::endl;
+    }
 }
 double rhombus::area() const {
-    if ( VectPropX(operator-(a_, b_), operator-(d_, c_)) == VectPropY(operator-(a_, b_), operator-(d_, c_)) &&
-    VectPropX(operator-(a_, d_), operator-(b_, c_)) == VectPropY(operator-(a_, d_), operator-(b_, c_)) &&
-    ScalProd(operator-(c_,a_), operator-(d_,b_)) == 0 ) {
-        return VectNorm(c_, a_) * VectNorm(d_, b_) / 2 ;
-    } else {
-        std::cout << "This is not a rhombus" << std::endl;
-    }
+    return VectNorm(c_, a_) * VectNorm(d_, b_) / 2;
 }
 
 point rhombus::center() const {
-    return point {((a_.x + b_.x + c_.x + d_.x) / 4), ((a_.y + b_.y + c_.y + d_.y) / 4)};
+    return point{((a_.x + b_.x + c_.x + d_.x) / 4), ((a_.y + b_.y + c_.y + d_.y) / 4)};
 }
-std::ostream& rhombus::print(std::ostream &os) const {
+
+std::ostream &rhombus::print(std::ostream &os) const {
     os << a_ << b_ << c_ << d_ << std::endl;
     return os;
 }
--- trapeze.cpp
+++ trapeze.cpp
@@ -5,9 +5,16 @@ trapeze::trapeze(): a_{0, 0}, b_{0, 0}, c_{0, 0}, d_{0, 0} {}
 trapeze::trapeze(const point &a, const point &b, const point &c, const point &d) : a_{a}, b_{b}, c_{c}, d_{d} {}
 trapeze::trapeze(std::istream &is) {
     is >> a_ >> b_ >> c_ >> d_;
+    if ( ( VectPropX(operator-(a_, b_), operator-(d_, c_)) == VectPropY(operator-(a_, b_), operator-(d_, c_)) || ( operator-(a_,b_).y == 0 && operator-(d_,c_).y == 0) ) ||
+         (VectPropX( operator-(b_, c_), operator-(a_, d_)) == VectPropX(operator-(b_, c_), operator-(a_, d_)) || ( operator-(b_,c_).y == 0 && operator-(a_,d_).y == 0) ) ) {
+        std::cout << "Correct" << std::endl;
+    } else {
+        std::cout << "Wrong" << std::endl;
+    }
 }
 double trapeze::area() const {
-    if ( VectPropX(operator-(a_, b_), operator-(d_, c_)) == VectPropY(operator-(a_, b_), operator-(d_, c_)) || ( operator-(a_,b_).y == 0 && operator-(d_,c_).y == 0) ) {
+    if (VectPropX(operator-(a_, b_), operator-(d_, c_)) == VectPropY(operator-(a_, b_), operator-(d_, c_)) ||
+        (operator-(a_, b_).y == 0 && operator-(d_, c_).y == 0)) {
         return ((VectNorm(a_, b_) + VectNorm(d_, c_)) / 2) * sqrt(
                 VectNorm(d_, a_) * VectNorm(d_, a_) - (
                         pow((
@@ -17,14 +24,26 @@ double trapeze::area() const {
                             ), 2)
                 )
         );
-    } else {
-        std::cout << "It`s not a trapeze" << std::endl;
+
+    } else if (VectPropX(operator-(b_, c_), operator-(a_, d_)) == VectPropX(operator-(b_, c_), operator-(a_, d_)) ||
+               (operator-(b_, c_).y == 0 && operator-(a_, d_).y == 0)) {
+        return ((VectNorm(b_, c_) + VectNorm(a_, d_)) / 2) * sqrt(
+                VectNorm(a_, b_) * VectNorm(a_, b_) - (
+                        pow((
+                                    (pow((VectNorm(a_, d_) - VectNorm(b_, c_)), 2) +
+                                     VectNorm(a_, b_) * VectNorm(a_, b_) - VectNorm(c_, d_) * VectNorm(c_, d_)) /
+                                    (2 * (VectNorm(a_, d_) - VectNorm(b_, c_)))
+                            ), 2)
+                )
+        );
     }
 }
+
 point trapeze::center() const {
-    return point {((a_.x + b_.x + c_.x + d_.x) / 4), ((a_.y + b_.y + c_.y + d_.y) / 4)};
+    return point{((a_.x + b_.x + c_.x + d_.x) / 4), ((a_.y + b_.y + c_.y + d_.y) / 4)};
 }
-std::ostream& trapeze::print(std::ostream &os) const {
+
+std::ostream &trapeze::print(std::ostream &os) const {
     os << a_ << b_ << c_ << d_ << std::endl;
     return os;
 }
